91b54f07f99ac1a013fcd196b40874501733d7f1
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2026-03-30 12:15:56 +0530

    Add state management to App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,20 @@ import './App.css';
 import Sidebar from "./Sidebar.jsx";
 import ChatWindow from "./ChatWindow.jsx";
 import { MyContext } from "./MyContext.jsx";
+import { useState } from 'react';
 
 function App() {
-  const providerValues = {};//passing values
+  const [prompt , setPrompt] = useState("");
+  const [reply , setReply] = useState(null);
+  const providerValues = {
+    prompt, setPrompt;
+    reply, setReply;
+  };
 
   return (
     <div className ='app'>
  <MyContext.Provider values={providerValues}>
-      <Sidebar></Sidebar>
+     <Sidebar></Sidebar>
       <ChatWindow></ChatWindow>
  </MyContext.Provider>
     </div>
