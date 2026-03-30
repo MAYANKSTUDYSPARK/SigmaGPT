52fd89b8191412377be39b2315f68beb8ea57a21
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2026-03-30 09:42:32 +0530

    Refactor Sidebar component structure and logo class

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,10 +1,9 @@
 import "./Sidebar.css";
 function Sidebar() {
   return(
-    <section className ="sidebar">
-    
-      <button>
-      <img src="/Frontend/src/assets/file_000000004f1471fa8297f31228302488.png" alt="gpt logo"></img>
+    <section className ="sidebar">    
+      <button>  
+      <img src="/Frontend/src/assets/file_000000004f1471fa8297f31228302488.png" alt="gpt logo" className="logo"></img>
    <i className="fa-solid fa-pen-to-square"></i>
       </button>
     
