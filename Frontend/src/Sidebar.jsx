e31d8aff9d19f5a4a159b38a51c3a29fdd5b4c83
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2026-03-30 09:38:42 +0530

    Refactor Sidebar component layout and content

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -2,13 +2,21 @@ import "./Sidebar.css";
 function Sidebar() {
   return(
     <section className ="sidebar">
-      {/* new chat button */}
+    
       <button>
       <img src="/Frontend/src/assets/file_000000004f1471fa8297f31228302488.png" alt="gpt logo"></img>
    <i className="fa-solid fa-pen-to-square"></i>
       </button>
-      {/* history */}
-      {/* sign */}
+    
+      <ul className="history">
+        <li>history1</li>
+        <li>history2</li>
+        <li>history3</li>
+      </ul>
+    
+  <div className="sign">
+    <p>By ScholarVerse Network &hearts;</p>
+  </div>
     </section>
   )
 }
