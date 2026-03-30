70238ca1fe1e1b5a0990f54c7ad69e22e782c122
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2026-03-30 10:32:32 +0530

    Refactor ChatWindow component layout and structure

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,8 +1,25 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 function ChatWindow(){
   return (
-    <div>
-      ChatWindow
+    <div className="ChatWindow">
+      <div className="navbar">
+        <span>SigmaGPT <i className="fa-solid fa-chevron-down"> </i></span>
+  <div className="userIconDiv" >
+ <span> <i className="fa-solid fa-user"></i></span>
+  </div>
+      </div>
+      <chat></chat>
+      <div className="chatInput">
+      <div className="userInput">
+        <input placeholder="Ask Anything">
+        </input>
+        <div id="submit"> <i className="fa-solid fa-paper-plane"></i></div>
+      </div>
+        <p className="info">
+          SigmaGPT Can Make Mistakes. Check Important Info. See Cookie Preferences.
+        </p>
+      </div>
     </div>
     )
 }
