4a06cb143e2d6de9595417169cee8d95cee2db47
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2026-03-31 06:56:43 +0530

    Integrate MyContext for chat input and replies
    Added context management for chat functionality and updated input handling.

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,6 +1,22 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+
 function ChatWindow(){
+const{  prompt , setPrompt , reply , setReply } = useContext(MyContext);
+const getReply = async () => {
+  const options {
+    method : "POST",
+      headers :{
+    "Content-Type" : "application/json"
+      },
+    body : {
+      message : prompt , 
+      threadId :
+    }
+  }
+}
+  
   return (
     <div className="ChatWindow">
       <div className="navbar">
@@ -12,9 +28,12 @@ function ChatWindow(){
       <chat></chat>
       <div className="chatInput">
       <div className="inputBox">
-        <input placeholder="Ask Anything">
+        <input placeholder="Ask Anything"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          >
         </input>
-        <div id="submit"> <i className="fa-solid fa-paper-plane"></i></div>
+        <div id="submit" onClick={getReply}> <i className="fa-solid fa-paper-plane"></i></div>
       </div>
         <p className="info">
           SigmaGPT Can Make Mistakes. Check Important Info. See Cookie Preferences.
