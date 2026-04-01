7c91de8179e249de715b5327459cb2a51e532b1d
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2026-04-01 06:25:54 +0530

    Update ChatWindow.jsx

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -5,10 +5,11 @@ import { useContext , useState , useEffect} from "react";
 import { ScaleLoader } from "react-spinners";
 
 function ChatWindow() {
-  const { prompt, setPrompt, reply, setReply, currThreadId , prevChats , setPrevChats } = useContext(MyContext);
+  const { prompt, setPrompt, reply, setReply, currThreadId , prevChats , setPrevChats , setNewChat } = useContext(MyContext);
   const [loading , setLoading] = useState(false);
   const getReply = async () => {
     setLoading(true);
+    setNewChat(false);
     console.log("message", prompt, "threadId", currThreadId);
 
     const options = {
